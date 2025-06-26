b10a0a4022f8c93490879b0e16ebab7401707eba
Daniel Del Castillo <teocchiii@gmail.com>
2025-06-26 12:12:39 -0500

    chore: initial commit.

---
+++ Americash/src/main/java/com/grupo4/americash/AmericashApplication.java
@@ -0,0 +1,13 @@
+package com.grupo4.americash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmericashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmericashApplication.class, args);
+	}
+
+}
