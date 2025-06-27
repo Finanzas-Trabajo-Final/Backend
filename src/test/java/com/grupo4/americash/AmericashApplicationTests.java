646ed33d2f7e3c228529a93c0a099ca1455d66d9
Daniel Del Castillo <teocchiii@gmail.com>
2025-06-26 22:34:05 -0500

    chore: initial commit

---
+++ src/test/java/com/grupo4/americash/AmericashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo4.americash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmericashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
